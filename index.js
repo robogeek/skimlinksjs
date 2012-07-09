550378a9ecb5b25ef98c4b7849398e9dbc329ce5
David Herron <david@davidherron.com>
2012-07-09 16:57:50 -0700

    Convert to using request module

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 
-var http = require('http');
-var url  = require('url');
-var util = require('util');
+var http    = require('http');
+var url     = require('url');
+var util    = require('util');
+var request = require('request');
 
 var apitoken = undefined;
 var apidmn   = "api-product.skimlinks.com";
@@ -12,82 +13,157 @@ exports.setup = function(token) {
 }
 
 exports.query = function(searchFor, start, rows, done) {
-    doQuery("/query", {
+    var qryobj = {
             key: apitoken,
             q: searchFor,
             start: start,
             format: "json",
-            rows: rows,
-            // version: 3
-            // fq: SOLR Filter Query
-        },
-        done
-        );
-}
-
-exports.categories = function(done) {
-    doQuery("/categories", {
-            key: apitoken,
-            format: "json"
-    }, done);
-}
-
-var doQuery = function(path, params, done) {
-    
-    var requrl = url.format(
-        params != undefined ? {
-            protocol: "http",
-            host: apidmn,
-            pathname: path,
-            query: params
-        }
-        : {
-            protocol: "http",
-            host: apidmn,
-            pathname: path
-        }
-        );
-    util.log(util.inspect(requrl));
-    var urlP = url.parse(requrl, false);
-    var options = {
-      host: urlP.host,
-      port: urlP.port,
-      path: urlP.pathname + urlP.search,
-      method: 'GET'
+            rows: rows
     };
-    var req = http.request(options, function(res) {
-      util.log('STATUS: ' + res.statusCode);
-      util.log('HEADERS: ' + util.inspect(res.headers));
-      res.setEncoding('utf8');
-      var data = "";
-      res.on('data', function (chunk) {
-          //util.log('BODY: ' + chunk);
-          data += chunk;
-      });
-      res.on('end',   function() {
-          // util.log('END: ' + data);
-          var json = JSON.parse(data);
+    
+    request({
+        method: 'GET',
+        uri: url.format({
+            protocol: "http",
+            host: apidmn,
+            pathname: "/query",
+            query: qryobj
+        })
+    }, function(error, response, body) {
+        if (error) { done(error); } // handle error
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          var json = JSON.parse(body);
           if (json.skimlinksProductAPI.products) {
               json.skimlinksProductAPI.products.forEach(function(item) {
                   if (item.price) {
                       var pr = item.price;
-                      //var cents = pr.substr(-2);
-                      //var dlr = pr.substr(0, pr.length-2);
-                      item.price = pr / 100; // dlr +'.'+ cents;
+                      item.price = pr / 100;
                   }
               });
           }
-          done(json);
-          //util.log(util.inspect(xmlDoc));
-      });
-      res.on('error', function(err) {
-          util.log('RESPONSE ERROR: ' + err);
-      });
+          done(null, json);
+        } else { // generate error
+            done({
+                reason: 'request error: '+ response.statusCode,
+                response: response,
+                body: body
+            });
+        }
     });
-    req.on('error', function(err) {
-        util.log('REQUEST ERROR: ' + err);
-    });
-    req.end();
 }
 
+exports.categories = function(done) {
+    request({
+        method: 'GET',
+        uri: url.format({
+            protocol: "http",
+            host: apidmn,
+            pathname: "/categories",
+            query: {
+                key: apitoken,
+                format: "json"
+            }
+        })
+    }, function(error, response, body) {
+        if (error) { done(error); } // handle error
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          var json = JSON.parse(body);
+          done(null, json);
+        } else { // generate error
+            done({
+                reason: 'request error: '+ response.statusCode,
+                response: response,
+                body: body
+            });
+        }
+    });
+}
+
+exports.merchantDomains = function(done, start, rows) {
+    request({
+        method: 'GET',
+        uri: url.format({
+            protocol: "http",
+            host: apidmn,
+            pathname: "/merchants/domains",
+            query: {
+                key: apitoken,
+                format: "json",
+                start: start,
+                rows: rows
+            }
+        })
+    }, function(error, response, body) {
+        if (error) { done(error); } // handle error
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          var json = JSON.parse(body);
+          done(null, json);
+        } else { // generate error
+            done({
+                reason: 'request error: '+ response.statusCode,
+                response: response,
+                body: body
+            });
+        }
+    });
+}
+
+exports.merchantSearch = function(done, searchFor, start, rows) {
+    request({
+        method: 'GET',
+        uri: url.format({
+            protocol: "http",
+            host: apidmn,
+            pathname: "/merchants/search",
+            query: {
+                key: apitoken,
+                format: "json",
+                search: searchFor,
+                start: start,
+                rows: rows
+            }
+        })
+    }, function(error, response, body) {
+        if (error) { done(error); } // handle error
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          var json = JSON.parse(body);
+          done(null, json);
+        } else { // generate error
+            done({
+                reason: 'request error: '+ response.statusCode,
+                response: response,
+                body: body
+            });
+        }
+    });
+}
+
+exports.merchantByCategory = function(done, category, start, rows) {
+    request({
+        method: 'GET',
+        uri: url.format({
+            protocol: "http",
+            host: apidmn,
+            pathname: "/merchants/category",
+            query: {
+                key: apitoken,
+                format: "json",
+                start: start,
+                rows: rows
+            }
+        })
+    }, function(error, response, body) {
+        if (error) { done(error); } // handle error
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          var json = JSON.parse(body);
+          done(null, json);
+        } else { // generate error
+            done({
+                reason: 'request error: '+ response.statusCode,
+                response: response,
+                body: body
+            });
+        }
+    });
+}
 
